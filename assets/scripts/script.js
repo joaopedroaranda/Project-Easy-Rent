5f1012001b31eb8bab1fa65a39d80d1389ce2f31
joao0918 <joaopedroaranda19@gmail.com>
2024-06-02 18:36:26 -0400

    update

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,42 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const products = document.querySelectorAll('.products input[type="checkbox"]');
-    const cartItems = document.getElementById('cartItems');
-    const checkoutButton = document.getElementById('checkoutButton');
+document.addEventListener('DOMContentLoaded', function() {
+    const checkboxes = document.querySelectorAll('.checkbox-group input[type="checkbox"]');
+    const continuarButton = document.getElementById('continuar-button');
+    
+    continuarButton.addEventListener('click', function(event) {
+        event.preventDefault(); // Impede o comportamento padrão do botão
 
-    products.forEach(product => {
-        product.addEventListener('change', updateCart);
-    });
-
-    function updateCart() {
-        cartItems.innerHTML = '';
-        products.forEach(product => {
-            if (product.checked) {
-                const listItem = document.createElement('li');
-                listItem.textContent = `${product.dataset.name}`;
-                cartItems.appendChild(listItem);
+        let checked = false;
+        checkboxes.forEach(function(checkbox) {
+            if (checkbox.checked) {
+                checked = true;
             }
         });
-    }
 
-    checkoutButton.addEventListener('click', () => {
-        const cartText = generateCartText();
-        sendToWhatsApp(cartText);
+        if (!checked) {
+            alert('Por favor, selecione ao menos um item.');
+            navigator.vibrate([200, 100, 200]); // Vibra o celular da pessoa
+        } else {
+            window.location.href = 'cadastro.html'; // Redireciona para a próxima página
+        }
     });
-
-    function generateCartText() {
-        let cartText = 'Itens Selecionados:\n';
-        products.forEach(product => {
-            if (product.checked) {
-                cartText += `${product.dataset.name} - $${product.dataset.price} ${product.dataset.mensage}\n`;
-            }
-        });
-        return cartText;
-    }
-
-    function sendToWhatsApp(cartText) {
-        const whatsappNumber = '+5567991105157'; // Substitua pelo número da sua empresa
-        const whatsappMessage = encodeURIComponent(cartText);
-        const whatsappURL = `https://wa.me/${whatsappNumber}?text=${whatsappMessage}`;
-        window.open(whatsappURL, '_blank');
-    }
 });
